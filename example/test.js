3b5e638d921f362ade920a10e0ff0b4a17f9c5b0
ChoCho66 <porkdie@gmail.com>
2024-03-26 15:38:01 +0800

    update index.qmd

---
+++ example/test.js
@@ -0,0 +1,7 @@
+
+
+a = 1
+
+// 把 a 變成字串
+
+console.log(typeof a + '')
---
+++ example/use_qmd_observable_files/libs/revealjs/plugin/reveal-myrevealjs/my-revealjs.js
@@ -0,0 +1,37 @@
+window.RevealMyrevealjs = function () {
+  return {
+    id: "RevealMyrevealjs",
+    init: function (deck) {
+      fontsizechange(deck)
+    },
+  };
+};
+
+function updateFontSize(delta) {
+  const fontSizeRegexp = /^(?<size>\d+)(?<unit>\D*)$/, // the beauty of named capturing groups 😍
+    fontSize = getComputedStyle(Reveal.getViewportElement()).getPropertyValue('--r-main-font-size'),
+    { size, unit } = fontSize.match(fontSizeRegexp).groups;
+
+  Reveal.getViewportElement().style.setProperty('--r-main-font-size', (parseInt(size) + delta) + unit);
+}
+function resetFontSize() {
+  Reveal.getViewportElement().style.removeProperty('--r-main-font-size');
+}
+
+const fontsizechange = function (Reveal) {
+  Reveal.addKeyBinding(
+    { keyCode: 189, key: '-', description: 'smaller font size' }, () => {
+      updateFontSize(-2)
+    }
+  )
+  Reveal.addKeyBinding(
+    { keyCode: 187, key: '+', description: 'larger font size' }, () => {
+      updateFontSize(+2)
+    }
+  )
+  Reveal.addKeyBinding(
+    { keyCode: 48, key: '0', description: 'reset font size' }, () => {
+      resetFontSize()
+    }
+  )
+};
